4331b64e5eee28270d4814970068ea4e4d5684e3
Anirudh Ganapathy <anirudhganapathy2003@gmail.com>
2022-07-08 18:50:53 +0530

    changed city_id to placeholder

--- config/conky/Auva/scripts/weather.sh
+++ config/conky/Auva/scripts/weather.sh
@@ -7,7 +7,7 @@
 # you can use this or replace with yours
 api_key=b59117c083dfa1d4e6cc3186a568fd26
 # get your city id at https://openweathermap.org/find and replace
-city_id=1264527
+city_id=xxxxxx
 
 
 url="api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${api_key}&cnt=5&units=metric&lang=en"
